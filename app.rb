0ba3ba31a03e0649430aeea82db618c7102ffd0f
twigz826 <thomastwigden@gmail.com>
2021-01-19 11:48:56 -0300

    Completed introducing params chapter

--- app.rb
+++ app.rb
@@ -9,6 +9,13 @@ get '/secret' do
   "You can totally do this!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
