126764e164d4c1a75f88a4677e1e6be00f0b923e
Dan T <dp.tyler@hotmail.co.uk>
2021-01-21 16:47:28 +0000

    web helpers added

--- Battle/spec/features/hitpoints_spec.rb
+++ Battle/spec/features/hitpoints_spec.rb
@@ -1,9 +1,6 @@
 feature 'seeing player hitpoints' do
   scenario 'expects players to be able to see hitpoints' do
-    visit('/')
-    fill_in :name_1, with: "Tom"
-    fill_in :name_2, with: "Dan"
-    click_button "Create players"
+    sign_in_and_play
     visit('/play')
     expect(page).to have_content "Dan: 60HP"
   end
--- Battle/spec/features/player_names_spec.rb
+++ Battle/spec/features/player_names_spec.rb
@@ -1,10 +1,6 @@
 feature 'filling in names in a form' do
   scenario 'expects players to fill in their names, submit that form and see those names on-screen' do
-    visit('/')
-    fill_in :name_1, with: "Tom"
-    fill_in :name_2, with: "Dan"
-    click_button "Create players"
-    #visit('/names')
+    sign_in_and_play
     expect(page).to have_content "Tom vs Dan"
   end
 end
