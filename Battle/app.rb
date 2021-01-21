00da378e4330d98ebea19cc0ecf9c3a0ac331f6b
Dan T <dp.tyler@hotmail.co.uk>
2021-01-21 16:16:13 +0000

    PRG Pattern added

--- Battle/app.rb
+++ Battle/app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/test' do
     "Testing infrastructure working!"
   end
@@ -11,7 +13,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @name_1, @name_2 = params[:name_1], params[:name_2]
+    session[:name_1] = params[:name_1]
+    session[:name_2] = params[:name_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name_1 = session[:name_1]
+    @name_2 = session[:name_2]
     erb(:play)
   end
 
