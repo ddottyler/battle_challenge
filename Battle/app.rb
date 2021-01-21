b246e049a619160240a4a76b39128ae2fa1ed06f
twigz826 <thomastwigden@gmail.com>
2021-01-21 12:55:37 -0300

    Entering players chapter completed

--- Battle/app.rb
+++ Battle/app.rb
@@ -2,8 +2,17 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/' do
+  get '/test' do
     "Testing infrastructure working!"
+  end
+
+  get '/' do
+    erb(:index)
+  end
+
+  post '/names' do
+    @name_1, @name_2 = params[:name_1], params[:name_2]
+    erb(:play)
   end
 
   run! if app_file == $0
--- Battle/spec/features/testing_infrastructure_spec.rb
+++ Battle/spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,6 @@
 feature 'check homepage has testing infrastructure working' do
   scenario 'should have testing infrastructure working' do
-    visit('/')
+    visit('/test')
     expect(page).to have_content "Testing infrastructure working!"
   end
 end
